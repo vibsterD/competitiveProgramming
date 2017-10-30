24b8929b007d7a7929ab4cc734f64b8001681411
Vibhu Dubey <vibhudubey97.a@gmail.com>
2017-10-30 21:14:52 +0530

    Add files via upload

---
+++ IARCS_problems_archive/WordListProb2.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<sstream>
+#include<set>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    string in="", r;
+    int n;
+    cin >> n;
+
+
+    char k = 39;
+    set <string> s;
+    //string l;
+    //cin >> l;
+    //getline(cin, l);
+    //cout << endl << l << endl <<endl;
+    cin.ignore();
+    for(int h=0; h<n; h++){
+        string temp;
+        //cin >> temp;
+        getline(cin, temp);
+        in = in+ " " + temp;
+        //cout << h << "  " << temp << endl;
+        /*for(int i=0; i<in.length(); i++){
+            in[i]= tolower(in[i]);
+            if(in[i]==k|| in[i]=='.' || in[i]==';' || in[i]==':' || in[i]==',')
+                in[i] = ' ';
+        }*/
+        /*istringstream iss(in);
+        while(iss>>r){
+            s.insert(r);
+        }*/
+    }
+    for(int i=0; i<in.length(); i++){
+            in[i]= tolower(in[i]);
+            if(in[i]==k|| in[i]=='.' || in[i]==';' || in[i]==':' || in[i]==',')
+                in[i] = ' ';
+    }
+    istringstream iss(in);
+    while(iss>>r){
+        s.insert(r);
+    }
+    cout << s.size() << endl;
+    for(auto x:s)
+        cout << x << endl;
+}
